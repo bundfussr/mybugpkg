66793a1d28ec4bbb996c4cf174c35f7ab3b83d97
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2022-11-28 15:03:44 +0100

    A R and man files

---
+++ R/allfuns.R
@@ -0,0 +1,12 @@
+
+#' A function
+#'
+#' @return `"a"`
+#' @export
+#'
+#' @keywords a_fun
+#'
+a_fun <- function() {
+  "a"
+}
+
---
+++ R/derive_advs_params.R
@@ -0,0 +1,14 @@
+#' Adds a Parameter for Mean Arterial Pressure
+#'
+#' @return 42
+#'
+#' @family der_prm_bds_findings
+#'
+#' @keywords der_prm_bds_findings
+#'
+#' @export
+#'
+derive_param_map <- function() {
+  42
+}
+
